beaf70f44e8bb3adf3eb5197bd39fff066570756
ckruder <carmenkchan@gmail.com>
2018-07-25 21:02:20 -0500

    testing heroku

--- ChatterBot.py
+++ ChatterBot.py
@@ -3,11 +3,14 @@ import tweepy
 import json
 import time
 
+from config import consumer_key, consumer_secret, access_token,
+access_token_secret
+
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+consumer_key = consumer_key
+consumer_secret = consumer_secret
+access_token = access_token
+access_token_secret = access_token_secret
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -16,4 +19,23 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 
 # Create a function that tweets
-# CODE GOES HERE
+def TweetOut(tweet_number):
+    api.update_status(
+        "Can't stop. Won't stop. Chatting! This is Tweet #%s!" %
+        tweet_number)
+
+
+# Create a function that calls the TweetOut function every minute
+counter = 0
+
+# Infinite loop
+while(counter < 50):
+
+    # Call the TweetQuotes function and specify the tweet number
+    TweetOut(counter)
+
+    # Once tweeted, wait 60 seconds before doing anything else
+    time.sleep(60)
+
+    # Add 1 to the counter prior to re-running the loop
+    counter = counter + 1
